c0512bf0953d91241e12ad454eb224647367d98a
Sebastian Webster <coolman16738@gmail.com>
2023-06-13 19:34:50 +1200

    update copyright year

--- components/AppCredits.js
+++ components/AppCredits.js
@@ -6,7 +6,7 @@ const AppCredits = () => {
     const {colors} = useTheme();
     return(
         <>
-            <Text style={{color: colors.tertiary, fontSize: 24, textAlign: 'center'}}>© SquareTable 2022</Text>
+            <Text style={{color: colors.tertiary, fontSize: 24, textAlign: 'center'}}>© SquareTable 2023</Text>
             <Text style={{color: colors.tertiary, fontSize: 24, textAlign: 'center', marginBottom: 10}}>All Rights Reserved</Text>
             <Text style={{color: colors.tertiary, fontSize: 20, textAlign: 'center', marginBottom: 3}}>Made by Sebastian Webster and Kovid Dev</Text>
             <Text style={{color: colors.tertiary, fontSize: 18, textAlign: 'center', marginBottom: 5}}>With huge help from Didula Semasinghe, Jacob Bowden, and Andrew Gossen</Text>
